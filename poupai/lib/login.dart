f7cfbd0c2c550aa6bc0fc8441787a79a4b3cd124
BrunoAlmeidaC <bruno.alveolar@gmail.com>
2025-12-02 21:08:25 -0300

    Pequenas alterações antes do merge

--- poupai/lib/login.dart
+++ poupai/lib/login.dart
@@ -16,7 +16,7 @@ class _LoginState extends State<Login> {
   bool isLoading = false;
 
   // Endereço base da API
-  final String baseUrl = "http://127.0.0.1:8000";//'http://10.0.2.2:8000'; // ajuste conforme ambiente
+  final String baseUrl = "http://10.0.2.2:8000";//'http://10.0.2.2:8000'; // ajuste conforme ambiente
 
   Future<void> loginUsuario() async {
     final email = emailController.text.trim();
